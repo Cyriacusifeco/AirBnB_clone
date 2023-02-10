cb894c32d03990bf33bf45b1300888479a2e01fa
Cyriacusifeco <cyriacusifeco@gmail.com>
2023-02-10 12:59:34 +0000

    Create Other essential classes for our beautiful website

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for Amenity
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class that inherits from BaseModel.
+    """
+
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module for state.
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    State class that inherits from BaseModel.
+    """
+
+    name = ""
